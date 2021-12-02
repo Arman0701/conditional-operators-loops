4195ddd037afb57b059a3ff93422eb6a9bc036b5
Arman773 <tadevosyan889@gmail.com>
2021-12-02 17:10:41 +0400

    added task 10 check prime number

---
+++ 9_is-prime-number.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// Insert a number. Print ‘yesʼ if the number is prime, ‘noʼ otherwise.
+
+function isPrime(num) {
+
+    if (num === 2) {
+        return true;
+    } else if (num > 1) {
+        for (var i = 2; i < num; i++) {
+            if (num % i !== 0) {
+                return true;
+            } else if (num === i * i) {
+                return false
+            } else {
+                return false;
+            }
+        }
+    } else {
+        return false;
+    }
+}
+  
+  console.log(isPrime(401));
+  console.log(isPrime(63));
